8d3b36a78815dfa020eeb953192da49a99f7ef03
Jaikeerthi-Srinivasa <jsrinivasa@hawk.iit.edu>
2020-09-30 11:45:29 -0500

    Commit

---
+++ Section 4/Spring/src/com/love2code/springdemo/Baseball.java
@@ -0,0 +1,10 @@
+package com.love2code.springdemo;
+
+public class Baseball implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		return "Spend 30 min on batting practice";
+	}
+
+}
